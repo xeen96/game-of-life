7773587802c9cbfcf69f0f6ae4ffc5013c5e172f
Xeen <162337536+xeen96@users.noreply.github.com>
2025-04-02 22:23:05 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,9 @@ import Game from './Game'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className={styles.content}>
-    <div className={styles.title}><h1>Game of Life</h1></div>
+    <div className={styles.title}>
+      <h1>Game of Life</h1>
+    </div>
     <Game/>
     </div>
   </StrictMode>
